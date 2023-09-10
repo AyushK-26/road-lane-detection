419b73bd89a653ec020fe17f4b29ca0d9b246444
Ayush <ayushkhanduri2612@gmail.com>
2023-09-10 19:34:13 +0530

    Added ROI function

--- lanes.py
+++ lanes.py
@@ -18,7 +18,8 @@ def region_of_interest(image):
     polygons = np.array([[(200, height), (1100, height), (550, 250)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
-    return mask
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
 
 
 # Loading image
@@ -26,5 +27,6 @@ image = cv2.imread('assets/test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
 
-cv2.imshow('Result', region_of_interest(canny))
+cropped_image = region_of_interest(canny)
+cv2.imshow('Result', cropped_image)
 cv2.waitKey(0)
